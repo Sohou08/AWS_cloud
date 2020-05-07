808675ec5228af40f53df206236f62a5dbf6e4eb
Sohou08 <51121757+Sohou08@users.noreply.github.com>
2020-05-07 12:45:51 +0200

    Update Recognition_python.py

--- recognition/Recognition_python.py
+++ recognition/Recognition_python.py
@@ -1,12 +1,12 @@
 #How access of the functionnality rekognition In AWS programmatily by using python
 
-"" First you need to create your own IAM
+//"" First you need to create your own IAM
 
 AWS Identity and Access Management (IAM) is a web service that helps you securely control access to AWS resources. 
-You use IAM to control who is authenticated (signed in) and authorized (has permissions) to use resources. 
-This one complete access to AWS rekognition API.
+/ You use IAM to control who is authenticated (signed in) and authorized (has permissions) to use resources. 
+/This one complete access to AWS rekognition API.
 
-Step
+/ Step
 
 Go to IAM--> User --> add user
 In set persmission , you have to attach certain quality to this user to access to AWS rekognition
@@ -19,10 +19,11 @@ Object and scene detection using AWS rekognition API
 
 How to access this functionality of AWS programming by using python. We can do that by access the boto 3 library 
 which the officel library by AWS.
-Check this site to get more information: boto3 Docs 1.9.98 documentation
+/Check this site to get more information: boto3 Docs 1.9.98 documentation
 In Docs/Available Services /Rekognition Section , if you scroll down, you will see the method  "detect_labels"
 
-Notice that AWS rekognition return differents kind of labels and results. ""
+Notice that AWS rekognition return differents kind of labels and results. ""// 
+
 
 ## Example
 
